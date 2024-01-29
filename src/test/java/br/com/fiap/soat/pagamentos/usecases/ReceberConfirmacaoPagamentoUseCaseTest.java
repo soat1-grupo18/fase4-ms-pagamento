a1c4f684ca6e5dd16a3f5d0f04bdda6979ea8fd7
Pablo Lima Dias <pabloidz@gmail.com>
2024-01-29 13:35:33 -0300

    Add API tests (#9)
    * Add API tests
    
    * Fix failing test
    
    * Add tests to use case
    
    * Rollback changes in application.properties

---
+++ src/test/java/br/com/fiap/soat/pagamentos/usecases/ReceberConfirmacaoPagamentoUseCaseTest.java
@@ -0,0 +1,45 @@
+package br.com.fiap.soat.pagamentos.usecases;
+
+import br.com.fiap.soat.pagamentos.entities.Pagamento;
+import br.com.fiap.soat.pagamentos.entities.Status;
+import br.com.fiap.soat.pagamentos.interfaces.gateways.PagamentosGatewayPort;
+import br.com.fiap.soat.pagamentos.usecases.model.ComandoDeConfirmacaoDePagamento;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReceberConfirmacaoPagamentoUseCaseTest {
+
+    @Mock
+    private PagamentosGatewayPort pagamentoGateway;
+
+    private ReceberConfirmacaoPagamentoUseCase receberConfirmacaoPagamentoUseCase;
+
+    @BeforeEach
+    void initUseCase() {
+        receberConfirmacaoPagamentoUseCase = new ReceberConfirmacaoPagamentoUseCase(pagamentoGateway);
+    }
+
+    @Test
+    void execute() {
+        var pagamentoId = UUID.randomUUID().toString();
+        var comando = new ComandoDeConfirmacaoDePagamento("payment.created", pagamentoId);
+        var pagamento = new Pagamento(pagamentoId, UUID.randomUUID().toString(), BigDecimal.valueOf(15), Status.PENDENTE, LocalDateTime.now().toString());
+        when(pagamentoGateway.obterPagamentoPorId(pagamentoId)).thenReturn(Optional.of(pagamento));
+        var result = receberConfirmacaoPagamentoUseCase.execute(comando);
+        assertEquals(result, String.format("Pagamento %s confirmado", pagamentoId));
+        verify(pagamentoGateway, times(1)).obterPagamentoPorId(pagamentoId);
+    }
+}
